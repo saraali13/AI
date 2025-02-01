3a92dc354835e6f5ddf327ab4d604155fd4b6c53
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-01 21:56:17 +0500

    Create Q1.py

---
+++ Lab 2/Q1.py
@@ -0,0 +1,53 @@
+import random
+
+
+class SecuritySystem:
+    def __init__(self):
+        self.components = {}
+        for i in range(9):
+            key = chr(65 + i)  # Convert number to letter (A to I)
+            self.components[key] = random.choice(["Safe", "Vulnerable"])  # Assign status
+
+    def initial_check(self):
+        print("\nInitial system status: \n", self.components)
+
+    def final_check(self):
+        print("\nFinal System Status: \n", self.components)
+
+
+class SecurityAgent(SecuritySystem):
+
+    def __init__(self):
+        super().__init__()
+        self.vulnerable_components = []
+
+    def scan_system(self):
+        for i in self.components:
+            if self.components[i] == "Vulnerable":
+                self.vulnerable_components.append(i)
+
+        print("\nScan Report:")
+        for i in self.components:
+            if i in self.vulnerable_components:
+                print(f"{i}: Warning Vulnerable!")
+
+            else:
+                print(f"{i}: Safe!")
+
+    def patch_system(self):
+        for i in self.vulnerable_components:
+            self.components[i] = "Safe"
+        print("\nPatched all vulnerable components!")
+
+
+sec_system = SecuritySystem()
+sec_agent = SecurityAgent()
+
+# Printing the initial state
+sec_system.initial_check()
+# scanning the system
+sec_agent.scan_system()
+# patching vulnerabilities
+sec_agent.patch_system()
+# printing the final state of the system after patching
+sec_system.final_check()

3bf3f4438470732eea9c19e5a58bb3f50cb3a6b3
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 17:47:14 +0500

    Update Q2.py

--- Lab 2/Q2.py
+++ Lab 2/Q2.py
@@ -14,22 +14,22 @@ class LoadBalancerSystem:
         print("\nFinal Servers Status: \n", self.servers)
 
 
-class LoadBalancerAgent(LoadBalancerSystem):
+class LoadBalancerAgent():
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self,system):
+        self.system=system
         self.overloaded_servers = []
         self.underloaded_servers = []
 
     def scan_system(self):
-        for i in self.servers:
-            if self.servers[i] == "Overloaded":
+        for i in self.system.servers:
+            if self.system.servers[i] == "Overloaded":
                 self.overloaded_servers.append(i)
-            elif self.servers[i] == "Underloaded":
+            elif self.system.servers[i] == "Underloaded":
                 self.underloaded_servers.append(i)
 
         print("\nScan Report:")
-        for i in self.servers:
+        for i in self.system.servers:
             if i in self.overloaded_servers:
                 print(f"{i}: Overloaded server!")
             elif i in self.underloaded_servers:
@@ -41,18 +41,18 @@ class LoadBalancerAgent(LoadBalancerSystem):
         for i in self.overloaded_servers:
             if self.underloaded_servers:
                 j = self.underloaded_servers.pop()
-                self.servers[i] = "Balanced"
-                self.servers[j] = "Balanced"
+                self.system.servers[i] = "Balanced"
+                self.system.servers[j] = "Balanced"
                 print(f"Balance is loaded from {i} to {j}")
 
-
-LB_Agent = LoadBalancerAgent()
+LB_system=LoadBalancerSystem()
+LB_Agent = LoadBalancerAgent(LB_system)
 
 # printing the initial state
-LB_Agent.initial_status()
+LB_system.initial_status()
 # scanning the system status
 LB_Agent.scan_system()
 # Balancing the load
 LB_Agent.balance_loader()
 # printing the final state of the system after balancing
-LB_Agent.final_status()
+LB_system.final_status()

840aa6b352640fe9d6ee257301167c9b328040af
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 17:54:29 +0500

    Update Q4.py

--- Lab 2/Q4.py
+++ Lab 2/Q4.py
@@ -16,19 +16,19 @@ class CyberSecuritySystem:
         print("\nFinal System Status: \n", self.components)
 
 
-class CyberSecurityAgent(CyberSecuritySystem):
+class CyberSecurityAgent:
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, system):
+        self.system = system
         self.vulnerable_components = []
 
     def scan_system(self):
-        for i in self.components:
-            if self.components[i] == "Low Risk Vulnerable" or self.components[i] == "High Risk Vulnerable":
+        for i in self.system.components:
+            if self.system.components[i] == "Low Risk Vulnerable" or self.system.components[i] == "High Risk Vulnerable":
                 self.vulnerable_components.append(i)
 
         print("\nScan Report:")
-        for i in self.components:
+        for i in self.system.components:
             if i in self.vulnerable_components:
                 print(f"{i}: Warning Vulnerable!")
 
@@ -38,22 +38,22 @@ class CyberSecurityAgent(CyberSecuritySystem):
     def patch_system(self):
 
         for i in self.vulnerable_components:
-            if self.components[i] == "Low Risk Vulnerable":
-                self.components[i] = "Safe"
+            if self.system.components[i] == "Low Risk Vulnerable":
+                self.system.components[i] = "Safe"
             else:
                 print(f"{i}: Premium service is required for patching High Risk Vulnerability")
         print("\nPatched all Low Risk Vulnerable components!")
 
 
 cy_sec_system = CyberSecuritySystem()
-cy_sec_agent = CyberSecurityAgent()
+cy_sec_agent = CyberSecurityAgent(cy_sec_system)
 
 # Printing the initial state
-cy_sec_agent.initial_check()
+cy_sec_system.initial_check()
 # scanning the system
 cy_sec_agent.scan_system()
 # patching vulnerabilities
 print("\n")
 cy_sec_agent.patch_system()
 # printing the final state of the system after patching
-cy_sec_agent.final_check()
+cy_sec_system.final_check()

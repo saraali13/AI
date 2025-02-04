e42234f1cdd4a2de9dbdb6e6d451f291c3597e43
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 18:02:07 +0500

    Update Q6.py

--- Lab 2/Q6.py
+++ Lab 2/Q6.py
@@ -1,4 +1,4 @@
-class FirefightingRobot:
+class FirefightingRobotSystem:
     def __init__(self):
         self.rooms = {'a': ' ', 'b': '🔥', 'c': '🔥', 'd': ' ', 'e': '🔥', 'f': ' ', 'g': '🔥', 'h': ' ', 'j': '🔥'}
         self.path = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j']
@@ -6,19 +6,27 @@ class FirefightingRobot:
     def initial_status(self):
         print("Initial Room Status:", self.rooms)
 
-    def move_and_extinguish(self):
-        # Move through rooms, extinguishing fires
-        for room in self.path:
-            print(f"Moved to Room {room}")
-            if self.rooms[room] == '🔥':
-                print(f"Extinguishing the Fire detected in Room {room}!!")
-                self.rooms[room] = ' '
-            print(f"Current Status: {self.rooms}")
-
     def final_status(self):
         print("Final Room Status:", self.rooms)
 
-FFR1=FirefightingRobot()
-FFR1.initial_status()
+
+class FirefightingRobotAgent:
+
+    def __init__(self, system):
+        self.system = system
+
+    def move_and_extinguish(self):
+        # Move through rooms, extinguishing fires
+        for room in self.system.path:
+            print(f"Moved to Room {room}")
+            if self.system.rooms[room] == '🔥':
+                print(f"Extinguishing the Fire detected in Room {room}!!")
+                self.system.rooms[room] = ' '
+            print(f"Current Status: {self.system.rooms}")
+
+
+sys=FirefightingRobotSystem()
+FFR1 = FirefightingRobotAgent(sys)
+sys.initial_status()
 FFR1.move_and_extinguish()
-FFR1.final_status()
+sys.final_status()
